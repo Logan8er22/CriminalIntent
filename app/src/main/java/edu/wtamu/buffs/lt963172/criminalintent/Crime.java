87e31ada65a69ca5fd81d5f2a632011ac3055e42
lt963172 <lthomas22@att.net>
2018-02-07 18:46:58 -0600

    Initial commit

---
+++ app/src/main/java/edu/wtamu/buffs/lt963172/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package edu.wtamu.buffs.lt963172.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by lt963172 on 2/7/2018.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
